1271f2bdf1dd603f211b0ebc04501f8632c00483
Muhammad Hasnain <hasnain0893@gmail.com>
2024-07-29 16:44:25 +0500

    file handling

--- file.js
+++ file.js
@@ -3,11 +3,27 @@ const fs = require("fs")
 
 
 //sync
-// fs.writeFileSync("./test.txt", "Hey there")
+//  const result =  fs.writeFileSync("./test.txt", "Hey there")
+// console.log(result);
 
 //async
 
 // fs.writeFile("./test.txt", "Hey there async", (error) => (error))
 
-const result = fs.readFileSync('./contact.txt', 'utf-8')
-console.log(result);
+// const result = fs.readFileSync('./contact.txt', 'utf-8')
+// console.log(result);
+
+
+
+// fs.readFile('./contact.txt', 'utf-8', (err, result) => {
+//     if (err) {
+//         console.log("error " + err);
+//     } else {
+//         console.log(result);
+//     }
+// })
+
+
+fs.appendFileSync("./test.txt", `${Date.now()}Hey there\n`)
+
+fs.copyFileSync("./test.txt", "./copy.txt")
